4678f29018f084f6b1b0692d25e8c29db70a46b2
KwokHoiYuet <103419876+KwokHoiYuet@users.noreply.github.com>
2022-04-18 02:56:28 +0800

    Update chatroom.php

--- Chat_Room/chatroom.php
+++ Chat_Room/chatroom.php
@@ -2,12 +2,12 @@
     session_start();
     if(isset($_SESSION[''unique_id])){
         include_once "config.php";
-        $outgoing_id = mysqli_real_escape_string($conn, $POST["outgoing_id"]);
-        $incoming_id = mysqli_real_escape_string($conn, $POST["incoming_id"]);
+        $outgoing_id = mysqli_real_escape_string($conn, $POST["out_id"]);
+        $incoming_id = mysqli_real_escape_string($conn, $POST["in_id"]);
         $message = mysqli_real_escape_string($conn, $POST["message"]);
         
         if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO message (incoming_msg_id, outgoing_msg_id, msg) VALUES ({$incoming_id},{$outgoing_id},'{$message}')")  
+            $sql = mysqli_query($conn, "INSERT INTO message (in_msg_id, out_msg_id, msg) VALUES ({$in_id},{$out_id},'{$message}')")  
         }else{
             header("../login.php")
         }
@@ -63,6 +63,8 @@
             </div>
 
             <form action="#" class="typing-area">
+                <input type="text" name="out_id" value="<?php echo $_SESSION['unique_id']:?>">
+                <input type="text" name="in_id" value="<?php echo $user_id:?>">
                 <input type="text" placeholder="Type a message here...">
                 <button><i class="fab fa-telegram-plane"></i></button>
             </form>
