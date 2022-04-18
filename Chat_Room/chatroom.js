13ef13575a563a81a15818f34dc7c5442a7c5df0
KwokHoiYuet <103419876+KwokHoiYuet@users.noreply.github.com>
2022-04-18 20:22:01 +0800

    Update chatroom.js

--- Chat_Room/chatroom.js
+++ Chat_Room/chatroom.js
@@ -1,44 +1,63 @@
-const form = document.querySelector(".typing_area"),
-inputField = form.querySelector(".input_field"),
-sendButton = form.querySelector("button");
+const form = document.querySelector(".typing-area"),
+in_id = form.querySelector(".inc_id").value,
+input_field = form.querySelector(".input-field"),
+sendButton = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box");
 
 form.onsubmit = (e)=>{
   e.preventDefault();
 }
 
-sendButton.onclick = ()=>{
-  #create XML object
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST","php/insert-message.php", true);
-  xhr.onload = ()=>{
-      if(xhr.readyState === XMLHttpRequest.DONE){
-        if(xhr.status === 200){
-        //insert once message in db and empty the input field
-            inputField.value ="";
-        }
-  }
-  
-  //send the data by ajax to php
-  let data = new FormData(form);
-  xhr.send(data);
+input_field.focus();
+input_field.onkeyup = ()=>{
+    if(input_field.value != ""){
+        sendButton.classList.add("active");
+    }else{
+        sendButton.classList.remove("active");
+    }
 }
 
-setInterval()=>{
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET","php/get_chat.php", true);
-  xhr.onload =()=>{
+sendButton.onclick = ()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/insert-chat.php", true);
+    xhr.onload = ()=>{
       if(xhr.readyState === XMLHttpRequest.DONE){
-          if(xhr.status === 200) {
-              let data = xhr.response;
-              if(!searchBar.classList.contains("active")){
-                userList.innerHTML = data;
+          if(xhr.status === 200){
+              input_field.value = "";
+              scrollToBottom();
+          }
+      }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
+setInterval(() =>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/get-chat.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+            let data = xhr.response;
+            chatBox.innerHTML = data;
+            if(!chatBox.classList.contains("active")){
+                scrollToBottom();
               }
           }
       }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("incoming_id="+incoming_id);
+}, 500);
+
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
   }
-  
-  //send the data by ajax to php
-  let data = new FormData(form);
-  xhr.send(data);
-},500);
 
