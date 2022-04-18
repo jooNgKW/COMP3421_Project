e2b899814fae94d7bc8c7af18fd0bda6aff85be6
KwokHoiYuet <103419876+KwokHoiYuet@users.noreply.github.com>
2022-04-18 20:18:12 +0800

    Update chatroom.php

--- Chat_Room/chatroom.php
+++ Chat_Room/chatroom.php
@@ -1,18 +1,18 @@
-<?php
-    session_start();
-
-    //insert message 
-    if(isset($_SESSION[''unique_id])){
-        include_once "config.php";
-        $outgoing_id = mysqli_real_escape_string($conn, $POST["out_id"]);
-        $incoming_id = mysqli_real_escape_string($conn, $POST["in_id"]);
-        $message = mysqli_real_escape_string($conn, $POST["message"]);
-        
-        if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO message (in_msg_id, out_msg_id, msg) VALUES ({$in_id},{$out_id},'{$message}')")  
-        }else{
-            header("../login.php")
-        }
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
+<?php include_once "header.php"; ?>'
+<?php 
+    $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+    if(mysqli_num_rows($sql) > 0){
+        $row = mysqli_fetch_assoc($sql);
+    }else{
+        header("location: users.php");
     }
 ?>
 
@@ -28,10 +28,10 @@
         <section class="chat-area">
             <header>
                 <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-                <img src="icon.jpg" alt="">
+                <img src="php/images/<?php echo $row['icon']; ?>" alt="">
                 <div class="details">
-                    <span>Coding Nepal</span>
-                    <p>Active now</p>
+                    <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+                    <p><?php echo $row['status']; ?></p>
                 </div>
             </header>
             
@@ -76,11 +76,11 @@
             <form action="#" class="typing-area">
                 <input type="text" name="out_id" value="<?php echo $_SESSION['unique_id']:?>" hidden>
                 <input type="text" name="in_id" value="<?php echo $user_id:?>" hidden>
-                <input type="text" placeholder="Type a message here...">
+                <input type="text" name="message" placeholder="Type a message here..." autocomplete="off">
                 <button><i class="fab fa-telegram-plane"></i></button>
             </form>
         </section>
     </div>
-
+<script src="javascript/users.js"></script>
 </body>
 </html>
