272cc407de9d268428a4f1598aa6be4531286023
KwokHoiYuet <103419876+KwokHoiYuet@users.noreply.github.com>
2022-04-18 21:22:30 +0800

    Update get_chat.php

--- 聊天室/get_chat.php
+++ 聊天室/get_chat.php
@@ -2,12 +2,12 @@
     session_start();
     if(isset($_SESSION['unique_id'])){
         include_once "config.php"
-        $out_id = mysqli_real_escape_string($conn, $_POST['out_id']);
-        $in_id = mysqli_real_escape_string($conn, $_POST['in_id']);
+        $out_id = mysqli_real_escape_string($link, $_POST['out_id']);
+        $in_id = mysqli_real_escape_string($link, $_POST['in_id']);
         $output = "";
 
         if(!empty($message)){
-            $sql = mysqli_query($conn, "SELECT * FROM messages WHERE (out_msg_id = {$out_id} AND in_msg_id = {$in_id})
+            $sql = mysqli_query($link, "SELECT * FROM messages WHERE (out_msg_id = {$out_id} AND in_msg_id = {$in_id})
                                         OR (out_msg_id = {$in_id} AND in_msg_id = {$out_id}) ORDER BY msg_id DESC")
             if(mysqli_num_rows($sql)>0){
                 while($row = mysqli_fetch_assoc($query)){
