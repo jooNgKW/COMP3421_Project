de8029b6af26fbf6e6f660f124aab009d50c91b6
NgManSing <ms-sam.ng@connect.polyu.hk>
2022-04-19 19:57:09 +0800

    chatroom to database

--- Chat_Room/chatroom.js
+++ Chat_Room/chatroom.js
@@ -4,10 +4,6 @@ const form = document.querySelector(".typing-area"),
 	sendButton = form.querySelector("button"),
 	chatBox = document.querySelector(".chat-box");
 
-form.onsubmit = (e) => {
-	e.preventDefault();
-};
-
 input_field.focus();
 input_field.onkeyup = () => {
 	if (input_field.value != "") {
@@ -17,20 +13,6 @@ input_field.onkeyup = () => {
 	}
 };
 
-sendButton.onclick = () => {
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", "insert_chat.php", true);
-	xhr.onload = () => {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				input_field.value = "";
-				scrollToBottom();
-			}
-		}
-	};
-	let formData = new FormData(form);
-	xhr.send(formData);
-};
 chatBox.onmouseenter = () => {
 	chatBox.classList.add("active");
 };
@@ -38,6 +20,10 @@ chatBox.onmouseenter = () => {
 chatBox.onmouseleave = () => {
 	chatBox.classList.remove("active");
 };
+
+function scrollToBottom() {
+	chatBox.scrollTop = chatBox.scrollHeight;
+}
 
 setInterval(() => {
 	let xhr = new XMLHttpRequest();
@@ -56,7 +42,3 @@ setInterval(() => {
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhr.send("in_id=" + in_id);
 }, 500);
-
-function scrollToBottom() {
-	chatBox.scrollTop = chatBox.scrollHeight;
-}
--- Chat_Room/insert_chat.php
+++ Chat_Room/insert_chat.php
@@ -1,14 +1,21 @@
 <?php 
     session_start();
+
     if(isset($_SESSION['unique_id'])){
-        include_once "config.php";
+        include_once "../config.php";
+
         $out_id = $_SESSION['unique_id'];
-        $ing_id = mysqli_real_escape_string($link, $_POST['in_id']);
+        $in_id = mysqli_real_escape_string($link, $_POST['in_id']);
         $message = mysqli_real_escape_string($link, $_POST['message']);
-        if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO messages (in_msg_id, out_msg_id, msg)
-                                        VALUES ({$in_id}, {$out_id}, '{$message}')") or die();
-        }
+
+        $sql = "INSERT INTO messages (in_msg_id, out_msg_id, msg) VALUES ('{$in_id}', '{$out_id}', '{$message}')";
+
+        if (mysqli_query($link, $sql)) {
+            header("location: ./chatroom.php");
+          } else {
+            echo "System Error, contact web admin for further assistance." . $sql . "<br>" . $link->error;
+          }
+
     }else{
         header("location: ../login.php");
     }
